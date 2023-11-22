84676af9d049a276975662684a36a40c0fa713ac
oskvr37 <oskarxpd@gmail.com>
2023-11-22 03:19:23 +0100

    'logging info for release'

--- enviz/__init__.py
+++ enviz/__init__.py
@@ -6,7 +6,7 @@ GRAY, CYAN, GREEN, RESET = "\033[90m", "\033[96m", "\033[92m", "\033[0m"
 
 
 logging.basicConfig(
-    level=logging.DEBUG,
+    level=logging.INFO,
     format=f"{GRAY}%(asctime)s {GREEN}%(levelname)s{RESET} <%(name)s> :: %(message)s",
     datefmt="[ %Y-%m-%d %X ]",
 )
